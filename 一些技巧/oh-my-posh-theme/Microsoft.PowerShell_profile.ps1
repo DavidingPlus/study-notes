1a46a040ec2c55658ceb5171bdbe33f3f9936576
DavidingPlus <davidingplus@qq.com>
2024-08-14 15:14:25 +0800

    fix tips and Microsoft.PowerShell_profile

--- 一些技巧/oh-my-posh-theme/Microsoft.PowerShell_profile.ps1
+++ 一些技巧/oh-my-posh-theme/Microsoft.PowerShell_profile.ps1
@@ -2,3 +2,4 @@ oh-my-posh --init --shell pwsh --config D:\scoop\apps\oh-my-posh\current\themes\
 Set-PSReadLineOption -PredictionSource None # 禁用预测 IntelliSense
 Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete # Tab键会出现自动补全菜单
 Import-Module posh-git # git的自动补全
+Set-Alias -Name ss -Value scoop-search # 为 scoop-search 命令设置别名
